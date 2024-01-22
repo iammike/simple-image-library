eccf2a847857b60e507751144dd0fe3225b8e717
Michael Collins <mike@iammike.org>
2024-01-22 10:19:01 -0500

    Initial Commit

---
+++ Simple Photo Viewer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Simple Photo Viewer
+//
+//  Created by Michael Collins on 1/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Simple Photo Viewer/Simple_Photo_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_Photo_ViewerApp.swift
+//  Simple Photo Viewer
+//
+//  Created by Michael Collins on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Simple_Photo_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
