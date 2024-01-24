7dc0a57e1871b6162fcab4e5dba2a5c9766a6c94
Michael Collins <mike@iammike.org>
2024-01-24 11:50:57 -0500

    Initialize audio session and pause before closing

--- Simple Photo Viewer/Simple_Photo_ViewerApp.swift
+++ Simple Photo Viewer/Simple_Photo_ViewerApp.swift
@@ -6,13 +6,27 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 @main
 struct Simple_Photo_ViewerApp: App {
-    
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+    }
+
+    init() {
+        setupAudioSession()
+    }
+    
+    private func setupAudioSession() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Failed to set audio session category. Error: \(error)")
         }
     }
 }
